519eed3074f9743d4ff5e0f34cf39b397cfe014f
Hachem Sfar <35065444+hachemsfar@users.noreply.github.com>
2024-06-18 07:27:28 +0200

    Delete content/Slide4.py

--- content/Slide4.py
+++
@@ -1,7 +0,0 @@
-st.title("Contact")
-st.header("Contact Tun'Up e.V.")
-st.text("""- Website: http://www.tunup.org/
-- Email: contact.munich@tunup.org""")
-st.header("Contact Presenters:")
-st.text("""- Hachem: hachem.sfar@tunup.org
-- Seif: seifeddine.ayadi@tunup.org""")
